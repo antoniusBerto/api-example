f2d8b41543d7acfa5e540288548582a06fde6d85
antoniusBerto <42685206+antoniusBerto@users.noreply.github.com>
2019-02-27 10:12:00 -0300

    Update SampleController.java

--- src/main/java/fiap/lab/microservices/apiexample/controller/SampleController.java
+++ src/main/java/fiap/lab/microservices/apiexample/controller/SampleController.java
@@ -7,7 +7,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
+@CrossOrigin( origins = "*" )
 @RequestMapping(value = "/api/samples", produces = MediaType.APPLICATION_JSON_VALUE)
 public class SampleController {
 
